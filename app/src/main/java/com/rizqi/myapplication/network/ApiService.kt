f451fff4892e659f798922c6ba5904a655ccb20b
rizqifakhri1 <rizqifakhri@gmail.com>
2022-04-20 11:00:35 +0700

    Making ApiClient.kt dan ApiService.kt

---
+++ app/src/main/java/com/rizqi/myapplication/network/ApiService.kt
@@ -0,0 +1,11 @@
+package com.rizqi.myapplication.network
+
+import com.rizqi.myapplication.model.GetAllMoviePopular
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("movie/popular?api_key=b4c17e0cc787b3a9f80b08cf1300bd72")
+    fun getAllMoviePopuler() : Call<GetAllMoviePopular>
+}
