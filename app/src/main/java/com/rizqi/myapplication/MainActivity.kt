abc554b414750efee90aeb957991ceae9bb7bee5
rizqifakhri1 <rizqifakhri@gmail.com>
2022-04-20 14:25:47 +0700

    Make Statusbar Transparant

--- app/src/main/java/com/rizqi/myapplication/MainActivity.kt
+++ app/src/main/java/com/rizqi/myapplication/MainActivity.kt
@@ -9,5 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) //Turn Off dark mode
+
+        setFullScreen(window)
+        lightStatusBar(window)
     }
 }
